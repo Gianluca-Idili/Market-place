a1ff1915376e2a8b9fbb0adfd3b6d9e42b51455c
FrancescoMasia <francesco.masia@hotmail.com>
2023-03-22 17:03:09 +0100

    sistemato carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,11 +15,11 @@
                 <div class="col-12 col-md-4">
                     <div class="main-pro bg-white shadow-card">
                         <div class="ms-4 p-3 bg-white text-black body-card"> 
-                            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                            <div id="item-{{$article->id}}" class="carousel slide" data-bs-ride="true">
                                 <div class="carousel-indicators">
-                                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                  <button type="button" data-bs-target="#item-{{$article->id}}" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                  <button type="button" data-bs-target="#item-{{$article->id}}" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                  <button type="button" data-bs-target="#item-{{$article->id}}" data-bs-slide-to="2" aria-label="Slide 3"></button>
                                 </div>
                                 <div class="carousel-inner">
                                   <div class="carousel-item active">
@@ -32,11 +32,11 @@
                                     <img src="http://picsum.photos//302" class="d-block w-100" alt="...">
                                   </div>
                                 </div>
-                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <button class="carousel-control-prev" type="button" data-bs-target="#item-{{$article->id}}" data-bs-slide="prev">
                                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                                   <span class="visually-hidden">Previous</span>
                                 </button>
-                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <button class="carousel-control-next" type="button" data-bs-target="#item-{{$article->id}}" data-bs-slide="next">
                                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                   <span class="visually-hidden">Next</span>
                                 </button>
