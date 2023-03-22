4297540d93965d3b7dced70e162316bb646f2ebc
Eros De Luca <erosdeluca124@gmail.com>
2023-03-22 15:58:48 +0100

    inserimento logo,colori

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,7 @@
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg bg-white mx-5 my-5 shadow">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('homepage')}}">Navbar</a>
+        <a class="navbar-brand" href="{{ route('homepage') }}"><img width="180"
+                src="\media\logo.png" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,52 +9,53 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('homepage') }}">Home</a>
+                    <a class="nav-link fs-5" href="{{ route('article.index') }}">Tutti gli articoli</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('article.create') }}">crea</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('article.index')}}">Tutti gli articoli</a>
-                </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown fs-5">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
-                  Categorie
+                        Categorie
                     </a>
+
                     <ul class="dropdown-menu">
                         @foreach ($categories as $category)
-                        <li><a class="dropdown-item" href=" {{route('category.show', compact('category'))}}">{{$category->name}}</a></li>
+                            <li><a class="dropdown-item"
+                                    href=" {{ route('category.show', compact('category')) }}">{{ $category->name }}</a>
+                            </li>
                         @endforeach
                     </ul>
-                @guest
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                      Benvenuto Ospite
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
-                            <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
-                        </ul>
-                    @else
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                          aria-expanded="false">
-                    Benvenuto {{Auth::user()->name}}
-                      </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{route('user.profile')}}">Profilo</a></li>
-                            <li><a class="dropdown-item" href=""
-                                    onclick="event.preventDefault();document.querySelector('#form-logout').submit()">Logout</a>
-                            </li>
-                            <form id="form-logout" method="POST" class="d-none" action="{{ route('logout') }}">@csrf
-                            </form>
-
-                        </ul>
-                    @endguest
-                </li>
             </ul>
+            <div class="nav-item mx-4 fs-5">
+                <a class="nav-link active" aria-current="page" href="{{ route('article.create') }}">Inserisci articolo</a>
+            </div>
+            @guest
+                <div class="nav-item dropdown me-3 fs-5">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                     Guest
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('register') }}">Registrati</a></li>
+                        <li><a class="dropdown-item" href="{{ route('login') }}">Login</a></li>
+                    </ul>
+                </div>
+                @else
+                <div class="nav-item dropdown me-3 fs-5">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                         {{ Auth::user()->name }}
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="{{ route('user.profile') }}">Profilo</a></li>
+                        <li><a class="dropdown-item" href=""
+                                onclick="event.preventDefault();document.querySelector('#form-logout').submit()">Logout</a>
+                        </li>
+                        <form id="form-logout" method="POST" class="d-none" action="{{ route('logout') }}">@csrf
+                        </form>
+
+                    </ul>
+                </div>
+            @endguest
         </div>
     </div>
 </nav>
