0e46258c6627275f4b421b668fd02c34dcbf67d7
Giambattista Papangelo <giambattistapapangelo@gmail.com>
2023-03-24 19:03:08 +0100

    sincronizzazione

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -20,11 +20,6 @@ class PublicController extends Controller
     }
     public function searchArticle(Request $request){
         $articles = Article::search($request->searched)->where('is_accepted', true)->paginate(10);
-        return view('article.index', compact('articles'));
-    }
-
-    public function searchArticles(Request $request){
-        $articles = Article::search($request->searched)->where('is_accepted', true)->paginate(10);
         return view('welcome', compact('articles'));
     }
 
