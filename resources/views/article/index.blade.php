c308de8f3be8f937112b04b69f29d21a131b1db6
Armando Detomaso <armandodetomaso@gmail.com>
2023-03-24 01:21:24 +0100

    sistemata pagina show

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
-    <x-header>
+    {{-- <x-header>
         <h1 class="mt-5 txtMain ms-5" >Esplora la nostra vasta selezione di articoli</h1>
-    </x-header>
+    </x-header> --}}
 
 
 @livewire('article-list')
