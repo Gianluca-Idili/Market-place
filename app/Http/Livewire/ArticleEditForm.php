a7ef461fd9f98402783905cb00201e44cb7cbab4
Armando Detomaso <armandodetomaso@gmail.com>
2023-03-31 17:44:17 +0200

    fatte alcune prove sul update

--- app/Http/Livewire/ArticleEditForm.php
+++ app/Http/Livewire/ArticleEditForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Article;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Illuminate\Support\Facades\Storage;
@@ -14,20 +15,12 @@ public $price;
 public $image;
 public $category_id;
 public $body;
-public $old_images = [];
 // public $temporary_images;
 public $images = []; 
 public $article;
+public $old_images;
 
-// public function updatedTemporaryImages(){
-//     if($this->validate([
-//         'temporary_images.*' => 'image|max:1024'
-//     ])){
-//         foreach($this->temporary_images as $image){
-//             $this->images[] = $image;
-//         }
-//     }
-// }
+
 public function update(){
     $this->article->update([
         'name' => $this->name,
@@ -35,6 +28,7 @@ public function update(){
         'price' => $this->price,
         'image' => $this->image,
         'category_id' => $this->category_id,
+
     ]);
     return redirect(route('user.profile'))->with('articleUpdate','studente aggiornato');
 }
@@ -45,10 +39,34 @@ public function mount(){
     $this->price = $this->article->price;
     $this->body = $this->article->body;
     $this->category_id = $this->article->category_id;
-    $this->old_images = Storage::url($this->article->id);
+
 }
     public function render()
     {
         return view('livewire.article-edit-form');
     }
+
+
+
+    public function updatedImages(){
+        if($this->validate([
+            'old_images.*' => 'image|max:1024'
+        ])){
+            foreach($this->old_images as $image){
+                $this->images[] = Storage::all()->article->images;
+            }
+        }
+    }
+
+
+    public function removeUpdatedImage($key){
+        if(in_array($key,array_keys($this->images))){
+            unset($this->images[$key]);
+        }
+    }
+
+
+
+
 }
+
--- resources/views/livewire/article-edit-form.blade.php
+++ resources/views/livewire/article-edit-form.blade.php
@@ -38,6 +38,39 @@
                 <span class="fst-italic text-danger small txtMain">{{ $message }}</span>
             @enderror
         </div>
+        {{-- @if (!empty($images))
+            <div class="row">
+                <div class="col-12">
+                    <p>Anteprima foto:</p>
+                    <div class="row border border-4 border-info rounded shadow py-4">
+                        @foreach ($images as $key => $image)
+                            <div class="col my-3">
+                                <div class="img-preview mx-auto shadow rounded"
+                                    style="background-image: url({{ $image->temporaryUrl() }});"></div>
+                                <button type="button" class="btn btn-danger shadow d-block text-center mt-2 mx-auto"
+                                    wire:click="removeImage({{ $key }})">{{ __('ui.delete') }}</button>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        @endif
+        <div class="mb-3">
+            <label for="images" class="form-label fs-5 txtMain">{{__('ui.images')}}</label>
+            <input type="file" wire:model="images" id="images" class="@error('images.*') is-invalid @enderror"
+                accept="image/*" multiple>
+            @error('images.*')
+                <span class="fst-italic text-danger small txtMain">{{ $message }}</span>
+            @enderror
+        </div> --}}
+
+        <div class="mb-3">
+            <input wire:model="old_images" type="file" multiple
+                class="form-control shadow @error('old_images.*') is-invalid @enderror" placeholder="Img" />
+            @error('old_images.*')
+                <p class=" text-danger small ">{{ $message }}</p>
+            @enderror
+        </div>
         @if (!empty($images))
             <div class="row">
                 <div class="col-12">
@@ -55,7 +88,7 @@
                 </div>
             </div>
         @endif
-        
+
         <button type="submit" class=" btn btn-addArt txtMain ">{{__('ui.insert')}}</button>
     </form>
 </div>
